62f42600b56398f23b263c7cdf24bff1b2b5aecb
Mohnish Mistry <12mm182@queensu.ca>
2018-01-23 14:31:55 -0500

    Initial Upload Based On Kyle's Creations

---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+    public Player_Controller thePlayer;
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<Player_Controller>();
+        lastPlayerPosition = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+}
---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpStrength;
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        myRigidBody.velocity = new Vector2(moveSpeed,myRigidBody.velocity.y);
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpStrength);
+        }
+
+        myAnimator.SetBool("grounded", grounded);
+	}
+}
+

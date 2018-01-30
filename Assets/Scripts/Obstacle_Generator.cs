13e79e5302292daf4487bf5e0e52e589575cc67c
Mohnish Mistry <12mm182@queensu.ca>
2018-01-29 22:59:23 -0500

    Fixed obstacle generation, added 70% chance to spawn a obstacle

--- Assets/Scripts/Obstacle_Generator.cs
+++ Assets/Scripts/Obstacle_Generator.cs
@@ -10,30 +10,24 @@ public class Obstacle_Generator : MonoBehaviour {
     public GameObject obstacle; 
 
     public float obstacleDistance;
-    public float platformWidth; 
+    public float platformWidth;
 
+    private Vector3 obstaclePosition; 
     // Use this for initialization
     void Start () {
         platformGeneratorScript = platformGenerator.GetComponent<Platform_Generator>();
     }
 
-// Update is called once per frame
-void Update () {
-        if (transform.position.x < generationPoint.transform.position.x)
+    public void Spawn()
+    {
+        if (Random.value > 0.3)
         {
-            transform.position = new Vector3((transform.position.x + platformGeneratorScript.distanceBetween), transform.position.y, transform.position.z); //Move Obstacle Generator to the start of the newly generated platform
             platformWidth = platformGeneratorScript.platformWidths[platformGeneratorScript.platformSelector];
-
-            if ((platformWidth > 3))
-            {
-                obstacleDistance = Random.Range(0.33f*platformWidth, 0.66f*platformWidth);
-
-                transform.position = new Vector3((transform.position.x + obstacleDistance), transform.position.y, transform.position.z); //Move Obstacle Generator to a random distance on the platform
-                Instantiate(obstacle, transform.position, transform.rotation);
-
-                transform.position = platformGenerator.transform.position; 
-            }
-        }
-
+            obstacleDistance = Random.Range(0.33f * platformWidth, 0.66f * platformWidth);
+            obstaclePosition = new Vector3((transform.position.x + obstacleDistance), transform.position.y, transform.position.z); //Move Obstacle Generator to a random distance on the platform
+            Instantiate(obstacle, obstaclePosition, transform.rotation);
+        } 
     }
+
 }
+
--- Assets/Scripts/Platform_Generator.cs
+++ Assets/Scripts/Platform_Generator.cs
@@ -14,18 +14,24 @@ public class Platform_Generator : MonoBehaviour {
     public float[] platformWidths;
     public Object_Pooler[] objectsPool;
 
-	// Use this for initialization
-	void Start () {
+    private Obstacle_Generator obstacleGeneratorScript;
+    public GameObject obstacleGenerator;
+
+    // Use this for initialization
+    void Start () {
         platformWidths = new float[objectsPool.Length];
 
         for (int i = 0; i < objectsPool.Length; i++)
         {
             platformWidths[i] = objectsPool[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        obstacleGeneratorScript = obstacleGenerator.GetComponent<Obstacle_Generator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         if(transform.position.x < generationPoint.position.x)
         {
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
@@ -39,6 +45,11 @@ public class Platform_Generator : MonoBehaviour {
             newPlatform.transform.rotation = transform.rotation;
 
             newPlatform.SetActive(true);
+
+            if (platformWidths[platformSelector] > 3)
+            {
+                obstacleGeneratorScript.Spawn();
+            } 
 
             transform.position = new Vector3((transform.position.x + (platformWidths[platformSelector])), transform.position.y, transform.position.z);
 

ddb881804d971f352784d36a98706324b99abb46
Kyle Ross <12kjr1@queensu.ca>
2018-02-07 16:02:18 -0500

    Obstacle destruction

---
+++ Assets/Scripts/Knight_Obstacle_Destroyer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_Obstacle_Destroyer : MonoBehaviour {
+    public GameObject player;
+    private Player_Controller controller;
+    private Collider2D obstacleCollider;
+    private Collider2D playerCollider;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+        controller = player.GetComponent<Player_Controller>();
+        obstacleCollider = GetComponent<Collider2D>();
+        playerCollider = player.GetComponent<Collider2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Physics2D.IsTouching(obstacleCollider, playerCollider) && controller.characterselect==1)
+        {
+            if (controller.ability == true)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}

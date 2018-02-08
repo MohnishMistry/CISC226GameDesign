c8cbb4891f7e641a391343ec45aca8783c0222ed
Kyle Ross <12kjr1@queensu.ca>
2018-02-08 14:06:39 -0500

    start of death

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform platformGenerator;
+    private Vector3 platformStartPoint;
+
+    public Player_Controller thePlayer;
+    private Vector3 playerStartPoint;
+	// Use this for initialization
+	void Start () {
+        platformStartPoint = platformGenerator.position;
+        playerStartPoint = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RestartGame()
+    {
+        StartCoroutine("RestartGameCo");
+    }
+
+    public IEnumerator RestartGameCo()
+    {
+        thePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        thePlayer.transform.position = playerStartPoint;
+        platformGenerator.position = platformStartPoint;
+        thePlayer.gameObject.SetActive(true);
+    }
+}

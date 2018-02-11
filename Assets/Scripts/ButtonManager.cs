e42d14e5e785713f8132474286a22c867f0e7a2c
Brad <12bchl@queensu.ca>
2018-02-11 18:20:28 -0500

    Start and Pause
    Start Menu is a separate scene that opens knight scene on "play". "Quit" doesn't do anythin in editor but should work in real game. Pausing with escape pauses the world but doesn't deactivate controls yet

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+    public void PlayButton(string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}

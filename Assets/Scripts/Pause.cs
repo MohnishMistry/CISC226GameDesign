3f0f0041187fd4971614d6fd45e147caea93e4ac
Mohnish Mistry <12mm182@queensu.ca>
2018-02-12 12:58:24 -0500

    Revert "Revert "Merge branch 'Title'""
    This reverts commit be25ba6860e197a6c7e13b1062d5b90189ba46ed.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+    public Transform Canvas;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Canvas.gameObject.activeInHierarchy == false)
+            {
+                Time.timeScale = 0;
+                Canvas.gameObject.SetActive(true);
+
+            }
+            else
+            {
+                Time.timeScale = 1;
+                Canvas.gameObject.SetActive(false);
+
+            }
+        }
+    }
+}

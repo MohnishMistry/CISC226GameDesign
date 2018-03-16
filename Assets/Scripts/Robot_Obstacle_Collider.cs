6c91407606decfe289f23ca006517b879dca5a66
Brad <12bchl@queensu.ca>
2018-03-16 18:02:55 -0400

    Robot Trannies

---
+++ Assets/Scripts/Robot_Obstacle_Collider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot_Obstacle_Collider : MonoBehaviour
+{
+    public GameObject player;
+    private Player_Controller controller;
+    private Collider2D obstacleCollider;
+    private Collider2D playerCollider;
+    public GameManager theGameManager;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        controller = player.GetComponent<Player_Controller>();
+        obstacleCollider = GetComponent<Collider2D>();
+        playerCollider = player.GetComponent<Collider2D>();
+        animator = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.tag == "Robot Wall")
+        {
+            if (controller.characterselect == 3 && controller.ability == true)
+            {
+                animator.SetBool("start", true);
+            }
+
+            if (Physics2D.IsTouching(obstacleCollider, playerCollider))
+            {
+                controller.death = true;
+            }
+      
+        }
+    }
+}

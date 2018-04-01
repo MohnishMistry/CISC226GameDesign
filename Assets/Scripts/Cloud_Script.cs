0a3e5ded896d88697a2444b6e829c3601aac9b55
Brad <12bchl@queensu.ca>
2018-04-01 16:19:19 -0400

    Clouds
    Still needs generation

---
+++ Assets/Scripts/Cloud_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud_Script : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(3.0f * Time.deltaTime, 0, 0);
+
+        if (player.transform.position.x - gameObject.transform.position.x > 15)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
